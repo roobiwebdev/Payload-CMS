801e981f5a82cc5db5ee5be799f3a9bf3e58d11a
roobiwebdev <mulaturhobot7@gmail.com>
2025-07-07 00:52:38 +0300

    allow public read access

--- src/collections/Hero.ts
+++ src/collections/Hero.ts
@@ -2,6 +2,9 @@ import type { CollectionConfig } from 'payload'
 
 const Hero: CollectionConfig = {
   slug: 'hero',
+  access: {
+    read: () => true,
+  },
   admin: {
     useAsTitle: 'mainTitle',
   },
